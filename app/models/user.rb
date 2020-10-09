7eb39e5e98707d4e624569ae8c4f1599b1216692
koujirou23 <kou23ilike5296@gmail.com>
2020-10-09 19:03:38 +0900

    ヘッダーの調整、修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, :password_confirmation
     validates :tall, format: { with:/\A[0-9]{3}+\z/ }
-    validates :weight, format: { with:/\A[0-9]{3}+\z/ }
+    validates :weight, format: { with:/\A[0-9]+\z/ }
   end
 
 end
--- db/migrate/20201008061355_devise_create_users.rb
+++ db/migrate/20201008061355_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false
-      t.string :password_confirmation, null: false
       t.integer :tall,               null: false
       t.integer :weight,             null: false
 
